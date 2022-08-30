e880dff1eb7dca4d690cdcbfcaf70aa571bef756
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-08-30 21:31:29 +0900

    #3.0 Your First Server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+};
+
+app.listen(PORT, handleListening); // 서버에 포트번호 부여
