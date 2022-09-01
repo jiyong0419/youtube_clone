f1fdd1e78ce6aa206ac4c6db6e061e6aa15025a4
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-01 18:06:25 +0900

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -6,9 +6,15 @@ const app = express();
 
 const PORT = 4000;
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => {
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
