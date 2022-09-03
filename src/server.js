4bebcac8473db6f0cdbe0ed315dce33a90881141
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-03 10:33:26 +0900

    #3.11 External Middlewares (morgan)

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
 "use strict";
 
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
 const PORT = 4000;
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
+const logger = morgan("dev");
 const protectedMiddleware = (req, res, next) => {
   if (req.url === "/protected") {
     return res.end();
@@ -20,9 +15,13 @@ const protectedMiddleware = (req, res, next) => {
     next();
   }
 };
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
 
+app.use(logger);
 app.use(protectedMiddleware);
-app.get("/", logger, handleHome);
+app.get("/", handleHome);
 
 const handleListening = () => {
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
