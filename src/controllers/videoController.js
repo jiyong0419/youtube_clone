0e6f501505879d60c105eb8d0c3b9c5197f6762f
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-06 13:58:16 +0900

    #5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,31 @@
 "use strict";
 export const trending = (req, res) => {
-  const videos = [];
+  const videos = [
+    {
+      title: "Video1",
+      rating: 0.1,
+      comments: 1,
+      createdAt: "1 minutes ago",
+      views: 1,
+      id: 1,
+    },
+    {
+      title: "Video2",
+      rating: 0.2,
+      comments: 1,
+      createdAt: "2 minutes ago",
+      views: 2,
+      id: 2,
+    },
+    {
+      title: "Video3",
+      rating: 0.3,
+      comments: 1,
+      createdAt: "3 minutes ago",
+      views: 3,
+      id: 3,
+    },
+  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 
