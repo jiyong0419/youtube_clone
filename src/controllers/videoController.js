ac201193d54243746fa6e0efd18f2e037edb458d
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-06 16:02:13 +0900

    #6.2 Edit Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,24 +26,18 @@ let videos = [
   },
 ];
 export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 
-export const edit = (req, res) => {
-  res.render("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const search = (req, res) => {
-  res.send("Search Video");
-};
-export const upload = (req, res) => {
-  res.send("Upload Video");
-};
-export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
-};
+export const postEdit = (req, res) => {};
