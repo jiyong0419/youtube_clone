6535bb9645e563d6eaf14f47524b866b04f17fc0
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-06 14:39:26 +0900

    #6.0 Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,38 @@
 "use strict";
+let videos = [
+  {
+    title: "Video1",
+    rating: 0.1,
+    comments: 1,
+    createdAt: "1 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Video2",
+    rating: 0.2,
+    comments: 1,
+    createdAt: "2 minutes ago",
+    views: 2,
+    id: 2,
+  },
+  {
+    title: "Video3",
+    rating: 0.3,
+    comments: 1,
+    createdAt: "3 minutes ago",
+    views: 3,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video1",
-      rating: 0.1,
-      comments: 1,
-      createdAt: "1 minutes ago",
-      views: 1,
-      id: 1,
-    },
-    {
-      title: "Video2",
-      rating: 0.2,
-      comments: 1,
-      createdAt: "2 minutes ago",
-      views: 2,
-      id: 2,
-    },
-    {
-      title: "Video3",
-      rating: 0.3,
-      comments: 1,
-      createdAt: "3 minutes ago",
-      views: 3,
-      id: 3,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 
 export const see = (req, res) => {
-  res.render("watch");
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 
 export const edit = (req, res) => {
