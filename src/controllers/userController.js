63396bca995e900acc97b6b89740b58eb4871b98
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-13 14:59:16 +0900

    #7.4 Status Codes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,12 +9,12 @@ export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   // 비밀번호 일치확인
   if (password !== password2) {
-    return res.render("join", { pageTitle: "Join", errorMessage: "Password confirmation does not match." });
+    return res.status(400).render("join", { pageTitle: "Join", errorMessage: "Password confirmation does not match." });
   }
   // username,email 중복확인
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.render("join", { pageTitle: "Join", errorMessage: "This username/email is already taken." });
+    return res.status(400).render("join", { pageTitle: "Join", errorMessage: "This username/email is already taken." });
   }
 
   await User.create({
