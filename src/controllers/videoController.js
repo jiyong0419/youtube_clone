199014fe8e93b89b2f34a5aacb704ea61158f53f
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-20 19:17:06 +0900

    #8.11 Video Owner

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 "use strict";
+import User from "../models/User";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
@@ -9,10 +10,11 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  const owner = await User.findById(video.owner);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  return res.render("watch", { pageTitle: video.title, video });
+  return res.render("watch", { pageTitle: video.title, video, owner });
 };
 
 export const getEdit = async (req, res) => {
@@ -44,13 +46,19 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
-  const { file } = req;
-  const { title, description, hashtags } = req.body;
+  const {
+    file,
+    session: {
+      user: { _id },
+    },
+    body: { title, description, hashtags },
+  } = req;
   try {
     await Video.create({
       title,
       description,
       videoUrl: file.path,
+      owner: _id,
       hashtags,
       // hashtags: Video.formatHashtags(hashtags),
     });
