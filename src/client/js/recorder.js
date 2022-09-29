e65ea5fdc976f7700807b290cb37697462f67343
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-29 09:01:24 +0900

    #13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,14 @@
 const startRecBtn = document.querySelector("#startRecBtn");
+const video = document.querySelector("#preview");
 
 const handleStartRec = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: false,
+    audio: false,
+    video: { width: 300, height: 200 },
   });
   console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 startRecBtn.addEventListener("click", handleStartRec);
