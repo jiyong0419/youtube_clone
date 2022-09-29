72099b862a651526a6e2ea09f62bb15810506007
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-30 01:02:33 +0900

    #13.3 Recording Video part Two

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,37 +1,47 @@
+// upload.pug에서 script
 const startRecBtn = document.querySelector("#startRecBtn");
 const video = document.querySelector("#preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {};
 
 const handleStopRec = () => {
-  startRecBtn.innerText = "Start Recording";
+  startRecBtn.innerText = "Download Recording";
   startRecBtn.removeEventListener("click", handleStopRec);
   startRecBtn.addEventListener("click", handleStartRec);
+
+  recorder.stop();
 };
 
 const handleStartRec = () => {
   startRecBtn.innerText = "Stop Recording";
   startRecBtn.removeEventListener("click", handleStartRec); // removeEventListener
   startRecBtn.addEventListener("click", handleStopRec);
-  const recorder = new MediaRecorder(stream); // new MediaRecorder는 녹화 기능을 제공 (인자로 유저의 미디어장치를받음)
+  recorder = new MediaRecorder(stream); // new MediaRecorder는 녹화 기능을 제공 (인자로 유저의 미디어장치를받음)
+  // recorder.stop()되었을떄 dataAvailable이벤트를 반환하는데 그것을 캐치
   recorder.ondataavailable = (e) => {
-    console.log(e);
-    console.log(e.data); // recorder.stop()에서 반환된 이벤트를 캐치한다
+    // console.log(e);
+    // console.log(e.data);
+    const videoFile = URL.createObjectURL(e.data); // 브라우저 메모리에서만 가능한 URL을 만들어준다
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
   //   console.log(recorder);  => state: 'inactive'
   recorder.start(); // 녹화시작
   //   console.log(recorder);  => state: 'recording'
-  setTimeout(() => {
-    recorder.stop(); // 5초후 녹화종료 , dataAvailable 이벤트를 반환한다
-  }, 5000);
+  // recorder.stop(); // 5초후 녹화종료 , dataAvailable 이벤트를 반환한다
 };
 
+// init은 초기설정이라 생각하자
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 300, height: 200 },
   }); // user의 미디어(오디오,비디오)장치를 불러온다 (유저의 미디어장치정보에서 데이터를 받아오므로 await)
-  console.log(stream);
   video.srcObject = stream; // 불러온 유저의 미디어장치를 video Element에 src로 설정해준다
   video.play(); // video를 재생시킨다.
 };
